bad4cb7b7fbc83d675181ce5b0a737d71b33aa52
Dmitrii Kopasov <58292035+mandarin-coffee@users.noreply.github.com>
2020-08-06 17:07:05 +0400

    dz3

--- resources/views/addNews.blade.php
+++ resources/views/addNews.blade.php
@@ -1,5 +1,9 @@
+@extends('layouts.index')
+
+@section('content')
 <p>Загаловок новости</p><input type="text">
 <p>Описание новости</p><textarea name="description" id="addDescNews" cols="30" rows="10"></textarea>
 <br>
 <br>
 <input type="submit" value="Добавить новость">
+@stop
